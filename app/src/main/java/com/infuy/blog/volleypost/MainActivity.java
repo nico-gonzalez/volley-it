1f4008801af68830094c1af64a9eebc4bd21113d
Nicolas Gonzalez <nico@Nicolass-MacBook-Pro.local>
2015-04-14 18:53:56 -0300

    wrapping example

--- app/src/main/java/com/infuy/blog/volleypost/MainActivity.java
+++ app/src/main/java/com/infuy/blog/volleypost/MainActivity.java
@@ -1,5 +1,7 @@
 package com.infuy.blog.volleypost;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.http.HttpResponseCache;
 import android.os.AsyncTask;
 import android.os.Build;
@@ -7,6 +9,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,8 +21,10 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.NetworkImageView;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
@@ -41,11 +46,17 @@ public class MainActivity extends ActionBarActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
     public static final String RANDOM_URL = "http://api.icndb.com/jokes/random";
+    public static final String IMAGE_URL = "http://www.los3dragones.com/imagenes/chucknorris/norris1.jpg";
 
     private TextView mJokeTextView;
+    private ImageView mHttpUrlConnectionImageView;
+    private ImageView mVolleyImageView;
+    private ImageView mVolleyImageLoaderImageView;
+    private NetworkImageView mNetworkImageView;
 
     private RequestQueue mRequestQueue;
 
+    private ImageLoader mImageLoader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,8 +69,105 @@ public class MainActivity extends ActionBarActivity {
         // a singleton to instantiate this once on a global scope
         mRequestQueue = Volley.newRequestQueue(this.getApplicationContext());
 
+        mHttpUrlConnectionImageView = (ImageView) findViewById(R.id.httpurlconnection_image_view);
+        new HttpURLConnectionImageAsyncTask(mHttpUrlConnectionImageView).execute(IMAGE_URL);
+
+        mVolleyImageView = (ImageView) findViewById(R.id.volley_image_request_view);
+
+        // you can use an image request and set the downloaded image on the onResponse callback
+        // or set an error image or placeholder when the call fails
+        ImageRequest imageRequest = new ImageRequest(IMAGE_URL, new Response.Listener<Bitmap>() {
+            @Override
+            public void onResponse(Bitmap response) {
+
+                mVolleyImageView.setImageBitmap(response);
+            }
+        },
+        0,
+        0,
+        ImageView.ScaleType.CENTER_CROP,
+        null,
+        new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // show an error drawable
+                mVolleyImageView.setImageDrawable(getDrawable(R.drawable.error));
+            }
+        }){
+            @Override
+            public Priority getPriority(){
+                return Priority.LOW;
+            }
+        };
+
+        // add the ImageRequest to the Volleys request queue
+        mRequestQueue.add(imageRequest);
+
+        // You can also user an ImageLoader with your custom ImageCache implementation
+        mVolleyImageLoaderImageView = (ImageView) findViewById(R.id.volley_imageloader_view);
+        mImageLoader = new ImageLoader(mRequestQueue, new LruBitmapCache(LruBitmapCache.getCacheSize(this)));
+        mImageLoader.get(IMAGE_URL, ImageLoader.getImageListener(mVolleyImageLoaderImageView, R.mipmap.ic_launcher, R.drawable.error));
+
+        // for super lazy programmers, you can use NetworkImageView in your View hierarchy that handles
+        // request lifecycle for you
+        mNetworkImageView = (NetworkImageView) findViewById(R.id.network_image_view);
+        mNetworkImageView.setImageUrl(IMAGE_URL, mImageLoader);
+        mNetworkImageView.setDefaultImageResId(R.mipmap.ic_launcher);
+        mNetworkImageView.setErrorImageResId(R.drawable.error);
+
     }
 
+    /**
+     * AsyncTask that downloads an image in a Worker Thread
+     */
+    public class HttpURLConnectionImageAsyncTask extends AsyncTask<String, Void, Bitmap>{
+
+        private ImageView mImageView;
+
+        public HttpURLConnectionImageAsyncTask(ImageView imageView){
+            this.mImageView = imageView;
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            HttpURLConnection connection = null;
+            try {
+                URL imageUrl = new URL(strings[0]);
+
+                connection = (HttpURLConnection) imageUrl.openConnection();
+                connection.setReadTimeout(10000);
+                connection.setConnectTimeout(15000);
+                connection.setRequestMethod("GET");
+                connection.setDoInput(true);
+                connection.connect();
+
+                if(connection.getResponseCode() ==  HttpURLConnection.HTTP_OK){
+
+                    return BitmapFactory.decodeStream(connection.getInputStream());
+                }
+
+                return null;
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                if(connection != null) connection.disconnect();
+            }
+
+            return null;
+        }
+
+        @Override
+        public void onPostExecute(Bitmap bitmap){
+            if (bitmap != null) mImageView.setImageBitmap(bitmap);
+        }
+
+    }
+
+    /**
+     * AsyncTask that uses HttpURLConnection to make a JSON api request
+     * It uses HttpResponseCache for response caching
+     */
     public class RandomJokeAsyncTask extends AsyncTask<Void, Void, String>{
 
         private boolean fetchedFromCache;
@@ -152,6 +260,10 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
+    /**
+     * Does the same request that above but using Volley.
+     * It uses Volleys Cache. We are forcing the response to be cached as the service returns a no cache header
+     */
     private void doVolley() {
 
 
@@ -215,27 +327,27 @@ public class MainActivity extends ActionBarActivity {
 
             };
 
-            // set the retry policy
+            // set the default retry policy
             request.setRetryPolicy(
                     new DefaultRetryPolicy());
 
-            // you can pass in your own retry policy
+            // or you can pass in your own retry policy
 
             /*
             request.setRetryPolicy(new RetryPolicy() {
                 @Override
                 public int getCurrentTimeout() {
-                    return 0;
+                    return 3000; // 3 seconds
                 }
 
                 @Override
                 public int getCurrentRetryCount() {
-                    return 0;
+                    return 3; // Attempt to retry at most 3 times
                 }
 
                 @Override
                 public void retry(VolleyError error) throws VolleyError {
-
+                     // i.e increase retry counter and determine if we reached the maximum retry attempts. If so, throw the error
                 }
             });
 
@@ -249,6 +361,10 @@ public class MainActivity extends ActionBarActivity {
         new RandomJokeAsyncTask().execute();
     }
 
+    /**
+     * Installs the HttpResponseCache
+     * Note this will only work on Android 4.0+
+     */
     private void enableHttpResponseCache() {
         try {
             long httpCacheSize = 10 * 1024 * 1024; // 10 MiB
@@ -259,6 +375,12 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
+    /**
+     * Fetches an entry value from the HttpResponseCache cache
+     * @param connection connection from which we need the cache
+     * @param uri uri to use to get the cache entry
+     * @return cache entry value as String
+     */
     private String fetchFromHTTPUrlConnectionCache(HttpURLConnection connection, URI uri) {
 
         try {
@@ -281,6 +403,10 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
+    /**
+     * Fetches entry from Volley cache
+     * @return boolean indicating the cache was hit
+     */
     private boolean fetchFromVolleyCache(){
 
         try{
@@ -301,11 +427,16 @@ public class MainActivity extends ActionBarActivity {
         return false;
     }
 
+    /**
+     * Invalidates both HttpResponseCache and Volley cache
+     * @param view
+     */
     public void invalidateCache(View view) {
 
         mRequestQueue.getCache().clear();
         try {
-            HttpResponseCache.getInstalled().delete();
+            HttpResponseCache cache = HttpResponseCache.getInstalled();
+            if(cache != null) cache.delete();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -317,14 +448,18 @@ public class MainActivity extends ActionBarActivity {
     protected void onStop(){
         super.onStop();
 
+        // You can cancel all the requests giving a scope object.
         mRequestQueue.cancelAll(this);
 
+        // you can cancel all the requests that meet a certain custom filter
+        /*
         mRequestQueue.cancelAll(new RequestQueue.RequestFilter() {
             @Override
             public boolean apply(Request<?> request) {
                 return request instanceof ImageRequest;
             }
         });
+        */
 
     }
 
